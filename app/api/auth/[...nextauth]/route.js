6e3fef3ecd58d0759463044d21ccfa828806865d
Mark Wild <mark.wild189@gmail.com>
2023-11-28 11:51:51 -0800

    add db creds

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
+//https://console.cloud.google.com/
 
+import { connectToDB } from '@utils/database';
+import User from '@models/user';
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -8,8 +11,38 @@ const handler = NextAuth({
       clientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+  async signIn({ profile }) {
+    try {
+      //serverless -> Lambda -> dynamodb
+      await connectToDB();
+
+      //check if user is already exists
+      const userExist = await User.findOne({
+        email: profile.email,
+      });
+
+      //if not create a new user
+      if (!userExist) {
+        await User.create({
+          email: profile.email,
+          userName: profile.name.replace(' ', '').toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
