3fe8fe6b93ba68d738c64740aa800a93718daa2b
Mark Wild <mark.wild189@gmail.com>
2023-12-16 17:23:11 -0800

    fix: margin cutoff

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,12 @@ const home = () => {
   return (
     <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
-        Discover & Share
+        Explore & Exchange
         <br className='max-md:hidden' />
         <span className='orange_gradient text-center'>AI Powered Prompts</span>
         <p className='desc text-center'>
-          Prompt share is an open-source AI prompting tool for modern world to
-          discover, create and share creative prompts
+          AI Prompt share is a AI prompting tool for modern era to explore,
+          create and share creative prompts
         </p>
       </h1>
       <Feed />
